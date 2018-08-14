9f699018fb8ac006cd72ba56295131732f7208e5
Alex <33516014+XanderBy@users.noreply.github.com>
2018-08-14 19:49:55 +0200

    La parte inferior la e implementado a medias ya solo quedaria si presionas un numero se seleccione
    version 0.0.29

--- Mi juego basico/src/dibujadoBasePantalla/ParteInferior.java
+++ Mi juego basico/src/dibujadoBasePantalla/ParteInferior.java
@@ -4,10 +4,14 @@ import java.awt.Color;
 import java.awt.Font;
 import java.awt.Graphics2D;
 
+import CosasBasicas.DibujosBasicos;
 import gestores.GestorJuego;
 import visual.SuperficieDibujo;
 
 public class ParteInferior {
+	
+	private static int posicionX;
+	private static int posicionY;
 	
 	public static void dibujar(Graphics2D g) 
 	{	
@@ -22,9 +26,56 @@ public class ParteInferior {
 		g.drawString("Armadura: ", SuperficieDibujo.arraySuperficie[0].getWidth()/200, (SuperficieDibujo.arraySuperficie[0].getHeight()/4) - SuperficieDibujo.arraySuperficie[0].getHeight()/50);
 		g.drawString(String.valueOf(GestorJuego.jugador.getArmadura()), SuperficieDibujo.arraySuperficie[0].getWidth()/25, (SuperficieDibujo.arraySuperficie[0].getHeight()/4) - SuperficieDibujo.arraySuperficie[0].getHeight()/50);
 		
+		for (int i = 0; i < 3; i++) {
+			try
+			{
+				if(i==0) 
+				{
+					posicionX=SuperficieDibujo.arraySuperficie[0].getWidth()/9 - SuperficieDibujo.arraySuperficie[0].getWidth()/30;
+					posicionY=(SuperficieDibujo.arraySuperficie[0].getHeight()/4) - SuperficieDibujo.arraySuperficie[0].getHeight()/27;
+					g.drawImage(GestorJuego.jugador.inventarioArray[i].sprite.imagen, posicionX, posicionY, GestorJuego.jugador.inventarioArray[i].sprite.introducirImagenesArray(GestorJuego.jugador.inventarioArray[i].sprite.imagen), null);
+					g.drawRect(posicionX, posicionY, GestorJuego.jugador.inventarioArray[i].ancho, GestorJuego.jugador.inventarioArray[i].alto);
+					
+				}
+			}catch (Exception e) {
+				// TODO: handle exception
+			}
+			try {
+				if(i==1)
+				{
+					posicionX=SuperficieDibujo.arraySuperficie[0].getWidth()/9;
+					posicionY=(SuperficieDibujo.arraySuperficie[0].getHeight()/4) - SuperficieDibujo.arraySuperficie[0].getHeight()/27;
+					g.drawImage(GestorJuego.jugador.inventarioArray[i].sprite.imagen, posicionX, posicionY, GestorJuego.jugador.inventarioArray[i].sprite.introducirImagenesArray(GestorJuego.jugador.inventarioArray[i].sprite.imagen), null);
+					g.drawRect(posicionX, posicionY, GestorJuego.jugador.inventarioArray[i].ancho, GestorJuego.jugador.inventarioArray[i].alto);
+				
+				}
+			}catch (Exception e) {
+				// TODO: handle exception
+			}
+			try{
+				if(i==2) 
+				{
+					posicionX=SuperficieDibujo.arraySuperficie[0].getWidth()/6 - SuperficieDibujo.arraySuperficie[0].getWidth()/45;
+					posicionY=(SuperficieDibujo.arraySuperficie[0].getHeight()/4) - SuperficieDibujo.arraySuperficie[0].getHeight()/27;
+					g.drawImage(GestorJuego.jugador.inventarioArray[i].sprite.imagen, posicionX, posicionY, GestorJuego.jugador.inventarioArray[i].sprite.introducirImagenesArray(GestorJuego.jugador.inventarioArray[i].sprite.imagen), null);
+					g.drawRect(posicionX, posicionY, GestorJuego.jugador.inventarioArray[i].ancho, GestorJuego.jugador.inventarioArray[i].alto);
+				
+				}
+			}catch (Exception e) {
+				// TODO: handle exception
+			}
+			
+				
+			
+			
+		}
+		
+		posicionX=0;
+		posicionY=0;
+		
 		g.setColor(Color.white);
-		g.drawRect(SuperficieDibujo.arraySuperficie[0].getWidth()/9, (SuperficieDibujo.arraySuperficie[0].getHeight()/4) - SuperficieDibujo.arraySuperficie[0].getHeight()/27, 25, 25);
 		g.drawRect(SuperficieDibujo.arraySuperficie[0].getWidth()/9 - SuperficieDibujo.arraySuperficie[0].getWidth()/30, (SuperficieDibujo.arraySuperficie[0].getHeight()/4) - SuperficieDibujo.arraySuperficie[0].getHeight()/27, 25, 25);
+		g.drawRect(SuperficieDibujo.arraySuperficie[0].getWidth()/9, (SuperficieDibujo.arraySuperficie[0].getHeight()/4) - SuperficieDibujo.arraySuperficie[0].getHeight()/27, 25, 25);
 		g.drawRect(SuperficieDibujo.arraySuperficie[0].getWidth()/6 - SuperficieDibujo.arraySuperficie[0].getWidth()/45, (SuperficieDibujo.arraySuperficie[0].getHeight()/4) - SuperficieDibujo.arraySuperficie[0].getHeight()/27, 25, 25);
 		
 	}
