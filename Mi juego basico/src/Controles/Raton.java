b0c9a8c066840c4ec722602fae2fa79acced2495
Alex <33516014+XanderBy@users.noreply.github.com>
2018-08-17 02:05:53 +0200

    El inventario esta completo ya solo falta la opcion de utilizar
    version 0.0.32

--- Mi juego basico/src/Controles/Raton.java
+++ Mi juego basico/src/Controles/Raton.java
@@ -18,257 +18,222 @@ import gestores.GestorOpciones;
 import objetos.Objetos;
 import visual.SuperficieDibujo;
 
-public class Raton implements MouseListener
-{	
-	
+public class Raton implements MouseListener {
+
 	public static Point puntaRaton;
 	private Rectangle puntero;
 	private SuperficieDibujo sd;
-	private GestorMenuPrincipal gmp=new GestorMenuPrincipal();
-	public static Objetos recuadroInventario=null;
-	public static Objetos objetoMover=null;
+	private GestorMenuPrincipal gmp = new GestorMenuPrincipal();
+	public static Objetos recuadroInventario = null;
+	public static Objetos objetoMover = null;
 	public Point posicionObjetos;
 	public static Dimension tamanoPantalla;
-	
-	
-	public void actualizar()
-	{	
-		
-		
-			SuperficieDibujo.arraySuperficie[0].actualizar();
-			puntaRaton=SuperficieDibujo.arraySuperficie[0].posicionRaton;
-			//System.out.println("posicion raton:" + puntaRaton);
-			if(puntaRaton==null) {
-				puntaRaton=new Point(0, 0);
-			}
-			puntero= new Rectangle((int) puntaRaton.getX(),(int) puntaRaton.getY(), 2, 2);
-			
-		
+
+	public void actualizar() {
+
+		SuperficieDibujo.arraySuperficie[0].actualizar();
+		puntaRaton = SuperficieDibujo.arraySuperficie[0].posicionRaton;
+		// System.out.println("posicion raton:" + puntaRaton);
+		if (puntaRaton == null) {
+			puntaRaton = new Point(0, 0);
+		}
+		puntero = new Rectangle((int) puntaRaton.getX(), (int) puntaRaton.getY(), 2, 2);
+
 	}
-	
+
 	@Override
-	public void mouseClicked(MouseEvent e)
-	{	
-		
-		
-		if(GestorMenuPrincipal.activoMenuPrincipal==true) 
-		{	
-			GestorJuego.contadorJugador=0;
-			GestorInventario.activoInventario=false;
-			GestorJuego.activoJuego=false;
-			GestorEscape.activoEscape=false;
-			GestorOpciones.activoOpciones=false;
-			
-			if(puntero.intersects(gmp.nuevaPartida)) {
-				GestorJuego.activoJuego=true;
-				Constantes.dondeEncuentraMenu=false;
+	public void mouseClicked(MouseEvent e) {
+
+		if (GestorMenuPrincipal.activoMenuPrincipal == true) {
+			GestorJuego.contadorJugador = 0;
+			GestorInventario.activoInventario = false;
+			GestorJuego.activoJuego = false;
+			GestorEscape.activoEscape = false;
+			GestorOpciones.activoOpciones = false;
+
+			if (puntero.intersects(gmp.nuevaPartida)) {
+				GestorJuego.activoJuego = true;
+				Constantes.dondeEncuentraMenu = false;
 			}
-			if(puntero.intersects(GestorMenuPrincipal.salir)) {
+			if (puntero.intersects(GestorMenuPrincipal.salir)) {
 				System.exit(0);
 			}
-			if(puntero.intersects(GestorMenuPrincipal.opciones)) {
-				GestorOpciones.activoOpciones=true;
-				
+			if (puntero.intersects(GestorMenuPrincipal.opciones)) {
+				GestorOpciones.activoOpciones = true;
+
 			}
 		}
-		
-		
-		if(GestorJuego.activoJuego==true) {
-			GestorMenuPrincipal.activoMenuPrincipal=false;
-			GestorInventario.activoInventario=false;
-			GestorEscape.activoEscape=false;
-			GestorOpciones.activoOpciones=false;
-			if(puntero.intersects(GestorJuego.rectangulo)) {
+
+		if (GestorJuego.activoJuego == true) {
+			GestorMenuPrincipal.activoMenuPrincipal = false;
+			GestorInventario.activoInventario = false;
+			GestorEscape.activoEscape = false;
+			GestorOpciones.activoOpciones = false;
+			if (puntero.intersects(GestorJuego.rectangulo)) {
 				System.out.println("Hola");
 			}
-				
-			
-			
-			
+
 		}
-		
-		
-		if(GestorEscape.activoEscape==true) 
-		{
-			GestorMenuPrincipal.activoMenuPrincipal=false;
-			GestorInventario.activoInventario=false;
-			GestorJuego.activoJuego=false;
-			GestorOpciones.activoOpciones=false;
-			if(puntero.intersects(GestorEscape.salirJuego)) {
+
+		if (GestorEscape.activoEscape == true) {
+			GestorMenuPrincipal.activoMenuPrincipal = false;
+			GestorInventario.activoInventario = false;
+			GestorJuego.activoJuego = false;
+			GestorOpciones.activoOpciones = false;
+			if (puntero.intersects(GestorEscape.salirJuego)) {
 				System.exit(0);
 			}
-			
-			if(puntero.intersects(GestorEscape.opciones)) {
-				
-				GestorOpciones.activoOpciones=true;
+
+			if (puntero.intersects(GestorEscape.opciones)) {
+
+				GestorOpciones.activoOpciones = true;
 			}
-			 
-			if(puntero.intersects(GestorEscape.salirMenu)) {
+
+			if (puntero.intersects(GestorEscape.salirMenu)) {
 				System.out.println("Hola");
-				Constantes.dondeEncuentraMenu=true;
-				GestorMenuPrincipal.activoMenuPrincipal=true;
-				
+				Constantes.dondeEncuentraMenu = true;
+				GestorMenuPrincipal.activoMenuPrincipal = true;
+
 			}
 		}
-		
-		
-		if(GestorOpciones.activoOpciones==true) 
-		{
-			GestorEscape.activoEscape=false;
-			GestorMenuPrincipal.activoMenuPrincipal=false;
-			
-			if(puntero.intersects(GestorOpciones.cerrar))
-			{
-				GestorOpciones.activoOpciones=false;
-				if(Constantes.dondeEncuentraMenu==true) 
-				{
-					GestorMenuPrincipal.activoMenuPrincipal=true;
-				}else {
-					GestorEscape.activoEscape=true;
+
+		if (GestorOpciones.activoOpciones == true) {
+			GestorEscape.activoEscape = false;
+			GestorMenuPrincipal.activoMenuPrincipal = false;
+
+			if (puntero.intersects(GestorOpciones.cerrar)) {
+				GestorOpciones.activoOpciones = false;
+				if (Constantes.dondeEncuentraMenu == true) {
+					GestorMenuPrincipal.activoMenuPrincipal = true;
+				} else {
+					GestorEscape.activoEscape = true;
 				}
 			}
-			
-			if(puntero.intersects(GestorOpciones.p1920x1080))
-			{
-				tamanoPantalla=new Dimension(1920, 1080);
+
+			if (puntero.intersects(GestorOpciones.p1920x1080)) {
+				tamanoPantalla = new Dimension(1920, 1080);
 				Constantes.arrayPantalla.get(0).setSize(tamanoPantalla);
 				Constantes.arrayPantalla.get(0).setLocation(0, 0);
-				//SuperficieDibujo.arraySuperficie[0].setSize(tamanoPantalla);
-				Constantes.ALTO=1080;
-				Constantes.ANCHO=1920;
+				// SuperficieDibujo.arraySuperficie[0].setSize(tamanoPantalla);
+				Constantes.ALTO = 1080;
+				Constantes.ANCHO = 1920;
 			}
 		}
-		
-		
-		if(GestorInventario.activoInventario==true) 
-		{
-			GestorMenuPrincipal.activoMenuPrincipal=false;
-			GestorJuego.activoJuego=false;
-			GestorEscape.activoEscape=false;
-			GestorOpciones.activoOpciones=false;
-			
-			if(GestorInventario.poderMover==true)
-			{
+
+		if (GestorInventario.activoInventario == true) {
+			GestorMenuPrincipal.activoMenuPrincipal = false;
+			GestorJuego.activoJuego = false;
+			GestorEscape.activoEscape = false;
+			GestorOpciones.activoOpciones = false;
+
+			if (GestorInventario.poderMover == true) {
+				for (int i=0; i<GestorInventario.recuadros.length; i++) {
+					System.out.println(GestorInventario.recuadros[i]);
 				
-				if(puntero.intersects(GestorInventario.moverObjeto02)) 
-				{
-					System.out.println(objetoMover);
-					//posicionObjetos=GestorInventario.moverObjeto02.getLocation();
-					//objetoMover.posicion=GestorInventario.moverObjeto02.getLocation();
-					for (int i = 0; i < GestorJuego.jugador.inventarioArray.length; i++) {
-						if(GestorJuego.jugador.inventarioArray[i]==objetoMover) {
-							GestorJuego.jugador.inventarioArray[i]=null;
-							break;
+						if (puntero.intersects(GestorInventario.recuadros[i])) {
+							System.out.println(objetoMover);
+							// posicionObjetos=GestorInventario.moverObjeto02.getLocation();
+							// objetoMover.posicion=GestorInventario.moverObjeto02.getLocation();
+
+							if (GestorJuego.jugador.inventarioArray[i] == null) {
+								for (int j = 0; j < GestorJuego.jugador.inventarioArray.length; j++) {
+									if (GestorJuego.jugador.inventarioArray[j] == objetoMover) {
+										GestorJuego.jugador.inventarioArray[j] = null;
+										break;
+									}
+								}
+								GestorJuego.jugador.inventarioArray[i] = objetoMover;
+								GestorInventario.poderMover = false;
+							}
+
 						}
-					}
-					
-					GestorJuego.jugador.inventarioArray[2]=objetoMover;
-					GestorInventario.poderMover=false;
 					
 				}
-			}
-			
-			try
-			{
-				if(puntero.intersects(GestorInventario.mover)) 
-				{
-					GestorInventario.poderMover=true;
-				}
-				if(puntero.intersects(GestorInventario.soltar)) 
-				{
-					for (int i = 0; i < GestorJuego.jugador.inventarioArray.length; i++) {
-						//GestorJuego.jugador.inventarioArray.remove(recuadroInventario);
-						if(GestorJuego.jugador.inventarioArray[i]==recuadroInventario)
-						{
-							GestorJuego.jugador.inventarioArray[i]=null;
-						}
-					}
-					
-					recuadroInventario.cogido=false;
-					posicionObjetos=new Point(AccionesBasicas.escalarPantallaX(200), AccionesBasicas.escalarPantallaY(200));
-					recuadroInventario.posicion=posicionObjetos;
-					
-					recuadroInventario=null;
-				}
-				if(puntero.intersects(GestorInventario.utilizar)) 
-				{
-					
-				}
-			}catch(NullPointerException ee) {
 				
 			}
-			
-			if(e.getButton()== e.BUTTON3)
-			{
-				for (int i = 0; i < GestorJuego.jugador.inventarioArray.length; i++) {
-					System.out.println(GestorJuego.jugador.inventarioArray[i] + " dwaaaaaaa");
+
+			try {
+				if (puntero.intersects(GestorInventario.mover)) {
+					GestorInventario.poderMover = true;
 				}
-				for (Objetos a : GestorJuego.jugador.inventarioArray) 
-				{	
+				if (puntero.intersects(GestorInventario.soltar)) {
+					for (int i = 0; i < GestorJuego.jugador.inventarioArray.length; i++) {
+						// GestorJuego.jugador.inventarioArray.remove(recuadroInventario);
+						if (GestorJuego.jugador.inventarioArray[i] == recuadroInventario) {
+							GestorJuego.jugador.inventarioArray[i] = null;
+						}
+					}
+
+					recuadroInventario.cogido = false;
+					posicionObjetos = new Point(AccionesBasicas.escalarPantallaX(200),
+							AccionesBasicas.escalarPantallaY(200));
+					recuadroInventario.posicion = posicionObjetos;
+
+					recuadroInventario = null;
+				}
+				if (puntero.intersects(GestorInventario.utilizar)) {
+
+				}
+			} catch (NullPointerException ee) {
+
+			}
+
+			if (e.getButton() == e.BUTTON3) {
+				for (Objetos a : GestorJuego.jugador.inventarioArray) {
 					try {
-						System.out.println(a + " dwaaaaaaa");
 						if (puntero.intersects(a.getAreaObjetoInfluencia())) {
 							System.out.println(23111111);
-							recuadroInventario=a;
-							objetoMover=a;
+							recuadroInventario = a;
+							objetoMover = a;
 							break;
 						}
-					}catch (NullPointerException ee) {
+					} catch (NullPointerException ee) {
 						// TODO: handle exception
-						
+
 					}
-					
+
 				}
 			}
-			
-			if(e.getButton()== e.BUTTON1)
-			{
-				for (Objetos a : GestorJuego.jugador.inventarioArray) 
-				{	
-					try
-					{
+
+			if (e.getButton() == e.BUTTON1) {
+				for (Objetos a : GestorJuego.jugador.inventarioArray) {
+					try {
 						if (!puntero.intersects(a.getAreaObjetoInfluencia())) {
-							
-							recuadroInventario=null;
+
+							recuadroInventario = null;
 						}
-					}catch (Exception ee) 
-					{
-						
+					} catch (Exception ee) {
+
 					}
-					
+
 				}
 			}
 		}
-		
-		
+
 	}
 
 	@Override
-	public void mouseEntered(MouseEvent e) 
-	{
+	public void mouseEntered(MouseEvent e) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
-	public void mouseExited(MouseEvent e) 
-	{
+	public void mouseExited(MouseEvent e) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
-	public void mousePressed(MouseEvent e) 
-	{
+	public void mousePressed(MouseEvent e) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
-	public void mouseReleased(MouseEvent e) 
-	{
+	public void mouseReleased(MouseEvent e) {
 		// TODO Auto-generated method stub
-		
+
 	}
-	
+
 }
--- Mi juego basico/src/gestores/GestorInventario.java
+++ Mi juego basico/src/gestores/GestorInventario.java
@@ -18,21 +18,25 @@ public class GestorInventario extends GestorPadre {
 
 	private static int contX = 0;
 	private static int contY = 0;
-	private static int contadorInventario=-1;
+	private static int contadorInventario = -1;
+	private static int contador = 0;
 
 	public static boolean poderMover = false;
-	public static Rectangle moverObjeto00;
-	public static Rectangle moverObjeto01;
-	public static Rectangle moverObjeto02;
-	public static Rectangle moverObjeto10;
-	public static Rectangle moverObjeto11;
-	public static Rectangle moverObjeto12;
-	public static Rectangle moverObjeto20;
-	public static Rectangle moverObjeto21;
-	public static Rectangle moverObjeto22;
-	public static Rectangle moverObjeto30;
-	public static Rectangle moverObjeto31;
-	public static Rectangle moverObjeto32;
+	public static Rectangle moverObjeto00 = null;
+	public static Rectangle moverObjeto01 = null;
+	public static Rectangle moverObjeto02 = null;
+	public static Rectangle moverObjeto10 = null;
+	public static Rectangle moverObjeto11 = null;
+	public static Rectangle moverObjeto12 = null;
+	public static Rectangle moverObjeto20 = null;
+	public static Rectangle moverObjeto21 = null;
+	public static Rectangle moverObjeto22 = null;
+	public static Rectangle moverObjeto30 = null;
+	public static Rectangle moverObjeto31 = null;
+	public static Rectangle moverObjeto32 = null;
+
+	public static Rectangle[] recuadros = { moverObjeto00, moverObjeto01, moverObjeto02, moverObjeto10, moverObjeto11,
+			moverObjeto12, moverObjeto20, moverObjeto21, moverObjeto22, moverObjeto30, moverObjeto31, moverObjeto32 };
 
 	public static Rectangle mover;
 	public static Rectangle soltar;
@@ -71,17 +75,17 @@ public class GestorInventario extends GestorPadre {
 		}
 
 		contY = 0;
-		
-		//System.out.println("------------------------------");
+
+		// System.out.println("------------------------------");
 		for (int i = 0; i < GestorJuego.jugador.inventarioArray.length; i++) {
-			//System.out.println("Objeto" + GestorJuego.jugador.inventarioArray[i]);
+			// System.out.println("Objeto" + GestorJuego.jugador.inventarioArray[i]);
 		}
-		//System.out.println("------------------------------");
+		// System.out.println("------------------------------");
 		// objetos dibujados
 		for (int i = 0; i < 3; i++) {
 			contX = SuperficieDibujo.arraySuperficie[0].getWidth() / 9;
 			contY += SuperficieDibujo.arraySuperficie[0].getHeight() / 4;
-			
+
 			for (int j = 0; j < 3; j++) {
 				if (contadorInventario == (GestorJuego.jugador.inventarioArray.length - 1)) {
 					break;
@@ -90,16 +94,18 @@ public class GestorInventario extends GestorPadre {
 				if (GestorJuego.jugador.inventarioArray[contadorInventario] != null) {
 					try {
 						Point posicionNueva = new Point(contX, contY);
-						//System.out.println(posicionNueva);
+						// System.out.println(posicionNueva);
 						GestorJuego.jugador.inventarioArray[contadorInventario].posicion = posicionNueva;
 						// System.out.println(posicionNueva);
 						g.drawImage(GestorJuego.jugador.inventarioArray[contadorInventario].spriteInventario.imagen,
 								GestorJuego.jugador.inventarioArray[contadorInventario].posicion.x,
 								GestorJuego.jugador.inventarioArray[contadorInventario].posicion.y,
-								GestorJuego.jugador.inventarioArray[contadorInventario].spriteInventario.introducirImagenesArray(
-										GestorJuego.jugador.inventarioArray[contadorInventario].spriteInventario.imagen),
+								GestorJuego.jugador.inventarioArray[contadorInventario].spriteInventario
+										.introducirImagenesArray(
+												GestorJuego.jugador.inventarioArray[contadorInventario].spriteInventario.imagen),
 								null);
-						//System.out.println(GestorJuego.jugador.inventarioArray[contadorInventario].posicion.x + "  " + GestorJuego.jugador.inventarioArray[contadorInventario].posicion.y);
+						// System.out.println(GestorJuego.jugador.inventarioArray[contadorInventario].posicion.x
+						// + " " + GestorJuego.jugador.inventarioArray[contadorInventario].posicion.y);
 						GestorJuego.jugador.inventarioArray[contadorInventario].areaObjetoInfluencia = new Rectangle(
 								GestorJuego.jugador.inventarioArray[contadorInventario].posicion.x,
 								GestorJuego.jugador.inventarioArray[contadorInventario].posicion.y,
@@ -112,9 +118,8 @@ public class GestorInventario extends GestorPadre {
 				contX += SuperficieDibujo.arraySuperficie[0].getWidth() / 9;
 			}
 
-			
 		}
-		contadorInventario=-1;
+		contadorInventario = -1;
 		contX = SuperficieDibujo.arraySuperficie[0].getWidth() / 9;
 		contY = 0;
 		if (Raton.recuadroInventario != null) {
@@ -193,7 +198,10 @@ public class GestorInventario extends GestorPadre {
 			contX += SuperficieDibujo.arraySuperficie[0].getWidth() / 9;
 			moverObjeto22 = new Rectangle(contX, contY, SuperficieDibujo.arraySuperficie[0].getWidth() / 9,
 					SuperficieDibujo.arraySuperficie[0].getWidth() / 9);
-
+			if(contador==0) {
+				AnadirRecuadros();
+				contador++;
+			}
 			contY = 0;
 			g.setColor(Color.ORANGE);
 			for (int i = 0; i < 3; i++) {
@@ -214,4 +222,32 @@ public class GestorInventario extends GestorPadre {
 
 	}
 
+	public void AnadirRecuadros() {
+		recuadros[0] = moverObjeto00;
+		recuadros[1] = moverObjeto01;
+		recuadros[2] = moverObjeto02;
+		recuadros[3] = moverObjeto10;
+		recuadros[4] = moverObjeto11;
+		recuadros[5] = moverObjeto12;
+		recuadros[6] = moverObjeto20;
+		recuadros[7] = moverObjeto21;
+		recuadros[8] = moverObjeto22;
+		recuadros[9] = moverObjeto30;
+		recuadros[10] = moverObjeto31;
+		recuadros[11] = moverObjeto32;
+	}
+	public void eliminarRecuadros() {
+		recuadros[0] = null;
+		recuadros[1] = null;
+		recuadros[2] = null;
+		recuadros[3] = null;
+		recuadros[4] = null;
+		recuadros[5] = null;
+		recuadros[6] = null;
+		recuadros[7] = null;
+		recuadros[8] = null;
+		recuadros[9] = null;
+		recuadros[10] = null;
+		recuadros[11] = null;
+	}
 }
